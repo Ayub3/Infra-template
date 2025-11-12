6f378ee80ea2f0ac6f1bed811dabcfb14675ca57
Ayub Mohamud <ayubmohamud@Ayubs-MacBook-Pro.local>
2025-11-12 17:36:18 +0000

    fix: update tags for vpc

--- terraform/env/dev/dev.tfvars
+++ terraform/env/dev/dev.tfvars
@@ -1,4 +1,4 @@
 region         = "eu-west-2"
 role_to_assume = "arn:aws:iam::499497677544:role/GithubOIDC"
-project        = "JummahCast-ECS"
+project        = "JummahCast-ECS-dev"
 env            = "dev"
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,11 @@ module "vpc" {
     cidr_block           = "10.0.0.0/16"
     enable_dns_hostnames = true
     enable_dns_support   = true
+
+    tags = {
+      env = var.env
+      project = "${var.project}-vpc"
+    }
   }
 
   public_cidr_block = ["10.0.0.0/24"]
