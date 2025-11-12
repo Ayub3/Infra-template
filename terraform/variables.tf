42b51e1187ff3be3d86c62220a40c501c7f9d555
Ayub Mohamud <ayubmohamud@Ayubs-MacBook-Pro.local>
2025-11-12 16:33:19 +0000

    fix: remove variables that are no longer referenced

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,14 +17,3 @@ variable "role_to_assume" {
   type        = string
   description = "oidc role to assume"
 }
-
-variable "state_bucket" {
-  type        = string
-  description = "name of state bucket"
-}
-
-variable "state_bucket_key" {
-  type        = string
-  description = "state bucket key"
-}
-
