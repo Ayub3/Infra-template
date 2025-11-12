c12d43f2e85fad1110d25b38992c7de907be8dfd
Ayub Mohamud <ayubmohamud@Ayubs-MacBook-Pro.local>
2025-11-12 14:17:13 +0000

    test workflow with simple deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,20 @@
-module "vpc" {
-  source = "git@github.com:Ayub3/TF-Modules.git//vpc"
-  aws_vpc = {
-    name                 = "${var.project}-${var.env}-vpc"
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support   = true
-  }
+# module "vpc" {
+#   source = "git@github.com:Ayub3/TF-Modules.git//vpc"
+#   aws_vpc = {
+#     name                 = "${var.project}-${var.env}-vpc"
+#     cidr_block           = "10.0.0.0/16"
+#     enable_dns_hostnames = true
+#     enable_dns_support   = true
+#   }
 
-  public_cidr_block = ["10.0.0.0/24"]
+#   public_cidr_block = ["10.0.0.0/24"]
+# }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
